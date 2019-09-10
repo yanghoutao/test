2aa910091e15e851aae047606f7a225251f0eeaf
yht <789@itcast>
2019-09-10 13:51:19 +0800

    第一次提交

---
+++ wwwww/src/main/java/ZhangJiaQi.java
@@ -0,0 +1,8 @@
+public class ZhangJiaQi {
+    public static void main(String[] args) {
+        System.out.println("张嘉琪");
+        System.out.println("张嘉琪");
+        System.out.println("张嘉琪");
+        System.out.println("张嘉琪");
+    }
+}
